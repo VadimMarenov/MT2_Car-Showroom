388a0b65ad12d1a73d40acf75e0fafa15f79c7af
Vadim <v.marenov@bk.ru>
2022-06-07 15:33:45 +0300

    First commit

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Dealer {
 
@@ -7,20 +9,24 @@ public class Dealer {
     static final int PURCHASE_TIME = 1000;
     static final int COUNT_OF_CARS = 10;
     List<Car> cars = new ArrayList<>();
+    ReentrantLock sellLock = new ReentrantLock(true);
+    Condition sellCondition = sellLock.newCondition();
 
-    public synchronized void sellCar() {
+    public void sellCar() {
         try {
+            sellLock.lock();
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
             if (cars.size() == 0) {
                 System.out.println("Машин нет");
-                    wait();
-
-                Thread.sleep(PURCHASE_TIME);
-                System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-                cars.remove(0);
+                sellCondition.await();
             }
+            Thread.sleep(PURCHASE_TIME);
+            System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
+            cars.remove(0);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        } finally {
+            sellLock.unlock();
         }
     }
 
@@ -29,16 +35,15 @@ public class Dealer {
             try {
                 Thread.sleep(BUILD_TIME);
                 cars.add(new Car());
-                System.out.print(Thread.currentThread().getName() + " выпустил автомобиль");
-                synchronized (this) {
-                    notify();
-                }
+                System.out.println(Thread.currentThread().getName() + " выпустил автомобиль");
+                sellLock.lock();
+                sellCondition.signal();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } finally {
+                sellLock.unlock();
             }
-            System.out.println();
         }
     }
-
 
 }
